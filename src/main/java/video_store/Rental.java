03e4ce8269a13168efa4b18385dd3e84b5bb05c3
Antonio de la Torre <antonio@codesai.com>
2019-03-06 01:51:28 +0100

    Replace increment with assignment

--- src/main/java/video_store/Rental.java
+++ src/main/java/video_store/Rental.java
@@ -22,15 +22,15 @@ public class Rental {
         double amount = 0;
         switch (movie.getPriceCode()) {
             case Movie.REGULAR:
-                amount += 2;
+                amount = 2;
                 if (getDaysRented() > 2)
                     amount += (getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                amount += getDaysRented() * 3;
+                amount = getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                amount += 1.5;
+                amount = 1.5;
                 if (getDaysRented() > 3)
                     amount += (getDaysRented() - 3) * 1.5;
                 break;
