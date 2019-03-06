5989b705cf3674dfefde2b7d34b7d436a3108e0e
Antonio de la Torre <antonio@codesai.com>
2019-03-06 01:48:35 +0100

    Create type

--- src/main/java/video_store/MovieType.java
+++ src/main/java/video_store/MovieType.java
@@ -2,6 +2,13 @@ package video_store;
 
 public class MovieType {
     public static MovieType create(int priceCode) {
+        if (priceCode == Movie.NEW_RELEASE) {
+            return new NewReleaseMovieType();
+        }
         return null;
+    }
+
+
+    private static class NewReleaseMovieType extends MovieType {
     }
 }
